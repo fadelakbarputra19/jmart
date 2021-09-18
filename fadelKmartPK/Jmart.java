78e1be400ae757364c037efc379723ff65545b46
Muhammad Fadel Akbar Putra <muhammad.fadel02@ui.ac.id>
2021-09-18 10:48:28 +0700

    menyelesaikan method create di jmart

--- fadelKmartPK/Jmart.java
+++ fadelKmartPK/Jmart.java
@@ -9,83 +9,11 @@ package fadelKmartPK;
  */
 public class Jmart
 {
-    /**
-    * Method getCustomer
-    */
-    public String getCustomer()
-    {
-        return "oop";
-    }
-       
-    /**
-    * Method getPromo
-    */
-    public static int getPromo()
-    {
-        return 0;
-    }
-       
-    /**
-    * Method getDiscountPercentage
-    */
-    public static float getDiscountPercentage(int before, int after)
-    {
-        if(before <= after){
-            return 0.0f;
-        }
-        else{
-            return (float) (before - after) / before * 100;
-        }
-    }
-
-    /**
-    * Method getDiscountedPrice
-    */
-    public static int getDiscountedPrice(int price, float discountPercentage)
-    {
-        if(discountPercentage > 100.0f){
-            return 0;
-        }else{
-            return (int) (price - (discountPercentage / 100) * price);
-        }
-    }
-       
-    /**
-    * Method getOriginalPrice
-    */
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage)
-    {
-        return (int) (discountedPrice/ (1 - (discountPercentage/100)));
-    }
-        
-    /**
-    * Method getCommissionMultiplier
-    */
-    public static float getCommissionMultiplier()
-    {
-        return 0.05f;
-    }   
-        
-    /**
-    * Method getAdjustedPeice
-    */
-    public static int getAdjustedPrice(int price)
-    {
-        return (int) (price + (getCommissionMultiplier() * price));
-    }
-        
-    /**
-    * Method getAdminFee
-    */
-    public static int getAdminFee(int price)
-    {
-        return (int) (price * getCommissionMultiplier());
-    }
     
     Product product;
     public static Product create(Product product)
     {   
-        return null;
+        return new Product("Manga", 1, true, new PriceTag(20000), ProductCategory.BOOK, new ProductRating());
     }
     
     /**
