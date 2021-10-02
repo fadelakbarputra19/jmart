561046e0bcbe522da2a8f784a1925b97d24fbe5f
Muhammad Fadel Akbar Putra <muhammad.fadel02@ui.ac.id>
2021-10-02 10:58:32 +0700

    CS-Modul 4-Lab PK

--- fadelKmartPK/Filter.java
+++ fadelKmartPK/Filter.java
@@ -3,10 +3,16 @@ package fadelKmartPK;
 import java.util.ArrayList;
 
 public class Filter {
-    public ArrayList<PriceTag> filterPriceTag(PriceTag[] list, int value, boolean less){
+    public static ArrayList<PriceTag> filterPriceTag(PriceTag[] list, double value, boolean less){
+        ArrayList<PriceTag> x = new ArrayList<>();
+        for(PriceTag i : list){
+            if(less && i.getAdjustedPrice() < value){
+                x.add(i);
+            }
+        }
         return null;
     }
     public void filterProductRating(ArrayList<ProductRating> list, double value, boolean less){
-        
+      
     }
 }
