4187a491f572677cea3f76075e96cda75cfaa3e4
Muhammad Fadel Akbar Putra <muhammad.fadel02@ui.ac.id>
2021-10-04 20:24:35 +0700

    PT-Modul 4-Lab PK

--- fadelKmartPK/Account.java
+++ fadelKmartPK/Account.java
@@ -1,5 +1,6 @@
 package fadelKmartPK;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Write a description of class Account here.
@@ -9,6 +10,8 @@ package fadelKmartPK;
  */
 public class Account extends Recognizable implements FileParser
 {
+    public static final String REGEX_EMAIL = "^\\w+([.&`~-]?\\w+)*@\\w+([.-]?\\w+)+$";
+    public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d][^-\\s]{8,}$";
     public String name;
     public String email;
     public String password;
@@ -20,6 +23,18 @@ public class Account extends Recognizable implements FileParser
         this.password = password;
     }
     
+    public boolean validate(){
+        Pattern a = Pattern.compile(REGEX_EMAIL);
+        Pattern b = Pattern.compile(REGEX_PASSWORD);
+        Matcher c = a.matcher(name);
+        Matcher d = b.matcher(email);
+        if(c.find() == true && d.find() == true){
+             return true;
+        }else{
+            return false;
+        }
+    }
+
     @Override
     public boolean read(String content){
         return true;
