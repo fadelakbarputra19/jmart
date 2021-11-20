fcb606ea5dfec534dcfdda3598c2239d440a74a7
Muhammad Fadel Akbar Putra <muhammad.fadel02@ui.ac.id>
2021-11-20 11:11:37 +0700

    CS-Modul 8-Lab PK

--- src/main/java/com/fadelJmartPK/controller/PaymentController.java
+++ src/main/java/com/fadelJmartPK/controller/PaymentController.java
@@ -1,5 +1,52 @@
 package com.fadelJmartPK.controller;
 
-public interface PaymentController {
-	
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fadelJmartPK.ObjectPoolThread;
+import com.fadelJmartPK.Payment;
+import com.fadelJmartPK.dbjson.JsonTable;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController implements BasicGetController<Payment> {
+
+    public static final long DELIVERED_LIMIT_MS = 0;
+    public static final long ON_DELIVERIY_LIMIT_MS = 0;
+    public static final long ON_PROGRESS_LIMIT_MS = 0;
+    public static final long WAITING_CONF_LIMIT_MS = 0;
+    public static JsonTable<Payment> paymentTable;
+    public static ObjectPoolThread<Payment> poolThread;
+
+    @Override
+    public JsonTable<Payment> getJsonTable() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @PostMapping("/{id}/accept")
+    public boolean accept(int id) {
+        return false;
+    }
+
+    @PostMapping("/{id}/cancel")
+    public boolean cancel(int id) {
+        return false;
+    }
+
+    @PostMapping("/create")
+    public Payment create(int buyerId, int productId, int productCount, String shipmentAddress, byte shipmentPlan) {
+        return null;
+    }
+
+    @PostMapping("/submit")
+    public boolean submit(int id, String receipt) {
+        return false;
+    }
+
+    @SuppressWarnings("unused")
+	private static boolean timekeeper(Payment payment) {
+        return false;
+    }
 }
